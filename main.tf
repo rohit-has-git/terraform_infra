3826b4d2bc0049447cdd0bc35614c4ef9a21523a
rohit-has-git <itz4rohit@gmail.com>
2025-07-01 06:22:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_security_group" "eks_nodes" {
 
 module "eks" {
     source = "terraform-aws-modules/eks/aws"
-    cluster_name = "new-cluster"
+    cluster_name = "ecm-cluster"
     cluster_version = "1.21"
     cluster_endpoint_public_access = true
     vpc_id = aws_vpc.main.id
